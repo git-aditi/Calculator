b45b8beed0a28870bfe1d2b9b4f940356734096f
Aditi Thakkar <aditithakkar1998@gmail.com>
2022-04-05 14:58:28 -0400

    Final Commit

--- Calculator/src/com/company/java/Calc.java
+++ Calculator/src/com/company/java/Calc.java
@@ -7,7 +7,7 @@ public class Calc implements ActionListener {
 
 	public double addition(double first, double second) {
 		return first + second; 
-	} 
+	}
  
 	public double subtraction(double first, double second) {
 		return first - second;
@@ -19,7 +19,7 @@ public class Calc implements ActionListener {
 
 	public double division(double first, double second) {
 		return first / second;
-
+ 
 	}
 
 	public double modulo(double first, double second) {
